178d749153aca2c42317c138911f2ceb97415107
Nikhil K C <nikhilkc96@gmail.com>
2019-03-31 18:40:43 +0530

    Update truffle.js
    change in truffle local host to eth0

--- truffle.js
+++ truffle.js
@@ -12,7 +12,7 @@ module.exports =
     	rinkeby: {
     	    provider: function() {
 				var mnemonic = "doctor multiply stereo hard turkey cigar eight flip excess enter blur exist"; //put ETH wallet 12 mnemonic code	
-				return new HDWalletProvider(mnemonic, "HTTP://127.0.0.1:7545");
+				return new HDWalletProvider(mnemonic, "HTTP://192.168.43.149:7545");
 		    },
 		    network_id: '5777',
 			from: '0x817eb0ce3540a67a0b5d2d30dc87b46b77801707', /*ETH wallet 12 mnemonic code wallet address*/
