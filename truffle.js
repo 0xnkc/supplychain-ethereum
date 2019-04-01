f73472df92afa4ab3c3af2649aad61a45a0a2fa9
Nikhil K C <nikhilkc96@gmail.com>
2019-04-02 00:24:16 +0530

    Update truffle.js

--- truffle.js
+++ truffle.js
@@ -11,11 +11,11 @@ module.exports =
 		},
     	rinkeby: {
     	    provider: function() {
-				var mnemonic = "doctor multiply stereo hard turkey cigar eight flip excess enter blur exist"; //put ETH wallet 12 mnemonic code	
+					var mnemonic = "trigger anchor mystery deposit potato diesel limit rain math depth action hole"; //put ETH wallet 12 mnemonic code	
 				return new HDWalletProvider(mnemonic, "HTTP://192.168.43.149:7545");
 		    },
 		    network_id: '5777',
-			from: '0x817eb0ce3540a67a0b5d2d30dc87b46b77801707', /*ETH wallet 12 mnemonic code wallet address*/
+				from: '0x6c427f3c532370dfa7b187e7c83b001f5bcaed34', /*ETH wallet 12 mnemonic code wallet address*/
 		}  
     }
 };
