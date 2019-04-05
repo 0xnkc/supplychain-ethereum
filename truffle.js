da00ba21c425e58efc532518ccfc3e8e93466a70
Nikhil K C <nikhilkc96@gmail.com>
2019-04-05 22:42:28 +0530

    ip change

--- truffle.js
+++ truffle.js
@@ -12,7 +12,7 @@ module.exports =
     	rinkeby: {
     	    provider: function() {
 					var mnemonic = "trigger anchor mystery deposit potato diesel limit rain math depth action hole"; //put ETH wallet 12 mnemonic code	
-				return new HDWalletProvider(mnemonic, "HTTP://192.168.43.149:7545");
+				return new HDWalletProvider(mnemonic, "HTTP://192.168.2.2:7545");
 		    },
 		    network_id: '5777',
 				from: '0x6c427f3c532370dfa7b187e7c83b001f5bcaed34', /*ETH wallet 12 mnemonic code wallet address*/
